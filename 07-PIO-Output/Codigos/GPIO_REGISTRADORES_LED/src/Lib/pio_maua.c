f8ff969f83af3d85384bc63017d6bc5ef0ad9b78
AmandaViviane <vivi_fabri@hotmail.com>
2016-05-10 21:52:37 -0300

    Atualizando Git - Aula 7

---
+++ 07-PIO-Output/Codigos/GPIO_REGISTRADORES_LED/src/Lib/pio_maua.c
@@ -0,0 +1,98 @@
+
+#include <asf.h>
+#include "pio_maua.h"
+
+
+// Função de Configuração dos Pinos
+
+void pin_config(void){
+	
+	// 29.17.4 PMC Peripheral Clock Enable Register 0
+	// 1: Enables the corresponding peripheral clock.
+	// ID_PIOA = 11 - TAB 11-1
+	PMC->PMC_PCER0 = 1 << ID_PIOA;
+	PMC->PMC_PCER0 = 1 << ID_PIOB;
+	PMC->PMC_PCER0 = 1 << ID_PIOC;
+
+	//31.6.1 PIO Enable Register
+	// 1: Enables the PIO to control the corresponding pin (disables peripheral control of the pin).
+	PIOA->PIO_PER |= (1 << PIN_LED_BLUE)|(1 << PIN_LED_GREEN );
+	PIOB->PIO_PER |= (1 << PIN_BOT_2   );
+	PIOC->PIO_PER |= (1 << PIN_LED_RED )|(1 << PIN_BOT_3     );
+	
+
+	// 31.6.46 PIO Write Protection Mode Register
+	// 0: Disables the write protection if WPKEY corresponds to 0x50494F (PIO in ASCII).
+	PIOA->PIO_WPMR = 0;
+	PIOB->PIO_WPMR = 0;
+	PIOC->PIO_WPMR = 0;
+	
+	// 31.6.4 PIO Output Enable Register
+	// value =
+	//	 	1 : Enables the output on the I/O line.
+	//	 	0 : do nothing
+	PIOA->PIO_OER |=  (1 << PIN_LED_BLUE )|(1 << PIN_LED_GREEN );
+	PIOC->PIO_OER |=  (1 << PIN_LED_RED );
+	
+	PIOB->PIO_ODR |=  (1 << PIN_BOT_2 );
+	PIOC->PIO_ODR |=  (1 << PIN_BOT_3 );
+	
+		// Ativar pull-up	
+	
+	PIOB->PIO_PUER |=  (1 << PIN_BOT_2 );
+	PIOC->PIO_PUER |=  (1 << PIN_BOT_3 );
+	
+		// Debouncing
+			
+	PIOB->PIO_IFDR |=  (1 << PIN_BOT_2 );
+	PIOC->PIO_IFDR |=  (1 << PIN_BOT_3 );
+	
+
+	
+}
+
+void PIOA_pin_set(void){
+	
+	PIOA->PIO_SODR = (1 << PIN_LED_BLUE )|(1 << PIN_LED_GREEN );
+
+	
+}
+
+void PIOC_pin_set(void){
+	
+	PIOC->PIO_SODR = (1 << PIN_LED_RED );
+	
+}
+
+void PIOA_pin_clear(void){
+	
+	PIOA->PIO_CODR = (1 << PIN_LED_BLUE )|(1 << PIN_LED_GREEN );
+
+	
+}
+
+void PIOC_pin_clear(void){
+	
+	PIOC->PIO_CODR = (1 << PIN_LED_RED );
+	
+}
+
+Bool PIOB_GetPinValue(void){
+	
+	if((PIOB->PIO_PDSR >> 3) & 1){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+Bool PIOC_GetPinValue(uint8_t pin_c){
+	
+	if(PIOC->PIO_PDSR & (1 << pin_c)){
+		return true;
+		}else{
+		return false;
+	}
+	
+}
+
---
+++ 07-PIO-Output/Codigos/GPIO_REGISTRADORES_LED/src/main.c
@@ -0,0 +1,99 @@
+/**
+ * IMT - Rafael Corsi
+ * 
+ * PIO - 07
+ *  Configura o PIO do SAM4S (Banco A, pino 19) para operar em
+ *  modo de output. Esse pino está conectado a um LED, que em 
+ *  lógica alta apaga e lógica baixa acende.
+*/
+
+#include <asf.h>
+#include <pio_maua.h>
+
+/*
+ * Prototypes
+ */
+
+/** 
+ * Definição dos pinos
+ * Pinos do uC referente aos LEDS.
+ *
+ * O número referente ao pino (PIOAxx), refere-se ao
+ * bit que deve ser configurado no registrador para alterar
+ * o estado desse bit específico.
+ *
+ * exe : O pino PIOA_19 é configurado nos registradores pelo bit
+ * 19. O registrador PIO_SODR configura se os pinos serão nível alto.
+ * Nesse caso o bit 19 desse registrador é referente ao pino PIOA_19
+ *
+ * ----------------------------------
+ * | BIT 19  | BIT 18  | ... |BIT 0 |
+ * ----------------------------------
+ * | PIOA_19 | PIOA_18 | ... |PIOA_0|
+ * ----------------------------------
+ */
+
+
+/**
+ * Main function
+ * 1. configura o clock do sistema
+ * 2. desabilita wathdog
+ * 3. ativa o clock para o PIOA
+ * 4. ativa o controle do pino ao PIO
+ * 5. desabilita a proteção contra gravações do registradores
+ * 6. ativa a o pino como modo output
+ * 7. coloca o HIGH no pino
+ */
+
+int main (void)
+{
+
+	/**
+	* Inicializando o clock do uP
+	*/
+	sysclk_init();
+	
+	/** 
+	*  Desabilitando o WathDog do uP
+	*/
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	pin_config();
+
+
+	/**
+	*	Loop infinito
+	*/
+		while(1){
+
+            /*
+             * Utilize a função delay_ms para fazer o led piscar na frequência
+             * escolhida por você.
+             */
+            //delay_ms();
+			
+			/*
+			delay_ms(500);
+			PIOA_pin_clear();
+			PIOC_pin_set();
+			delay_ms(500);
+			PIOA_pin_set();
+			PIOC_pin_clear();
+			*/
+			
+			if(PIOB_GetPinValue(PIN_BOT_2)){
+				PIOA_pin_set();
+			}else{
+				PIOA_pin_clear();
+			}
+			
+			if(PIOC_GetPinValue(PIN_BOT_3)){
+				PIOC_pin_set();
+				}else{
+				PIOC_pin_clear();
+			}		
+	}
+}
+
+
+
